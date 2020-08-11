b480925d92ed80cd96cf3c85a0172981d3942502
karthick1999 <55175441+karthick1999@users.noreply.github.com>
2020-08-11 11:39:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const minEl=document.querySelector('.min'); 
+const secEl=document.querySelector('.sec'); 
+const hourEl=document.querySelector('.hour'); 
+
+setInterval(()=>{
+    const date=new Date; 
+    console.log(date); 
+    const secDeg=date.getSeconds()/60*360-90;  
+    const minDeg=date.getMinutes()/60*360-90;  
+    const hourDeg=date.getHours()/12*360-90; 
+    secEl.style.transform=`rotate(${secDeg}deg)` 
+    minEl.style.transform=`rotate(${minDeg}deg)` 
+    hourEl.style.transform=`rotate(${hourDeg}deg)`
+},1000)
